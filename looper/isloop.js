bce0e024a92449c4874225efc13c3fc3357bdf4e
Kadecahe <kadecahe@gmail.com>
2020-10-28 20:33:40 -0400

    my version of isLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,6 +2,33 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  //does this contain a loop
+  //use a new Map
+  //if there is no linkedlist.head.value in the map, make one and have that hold the next one
+  //if there is that value already have it return true
+  //if its null have it return false
+
+  const linkedMap = new Map();
+
+  let key = linkedlist.head.value,
+    value = linkedlist.head.next,
+     bool = false;
+
+  linkedMap.set(key, value)
+
+  while(!bool) {
+    let nextKey = value.value,
+      nextValue = value.next;
+
+    if(linkedMap.has(nextKey)) return !bool;
+
+    linkedMap.set(nextKey, nextValue)
+
+    nextKey = nextKey.value;
+    value = nextValue.next;
+
+    if(!value) return bool;
+  }
 
 };
 
